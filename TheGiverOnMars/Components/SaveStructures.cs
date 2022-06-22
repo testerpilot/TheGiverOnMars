af7e9094e18e96078d59def91200381f3f762d70
timrkelley1 <timrkelley1@gmail.com>
2022-06-22 13:14:21 -0500

    Added untracked files

---
+++ TheGiverOnMars/Components/SaveStructures.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TheGiverOnMars.Dictionaries;
+
+namespace TheGiverOnMars.Components
+{
+    public class MapEntrySaveData
+    {
+        public string Name { get; set; }
+        public List<MapDictionaryEntry.ChestObjectEntry> ChestEntries { get; set; }
+    }
+
+    public class MapSaveData
+    {
+        public string CurrentMap { get; set; }
+        public List<MapEntrySaveData> MapSaves { get; set; }
+    }
+
+    public class PlayerSaveData
+    {
+        public float PosX { get; set; }
+        public float PosY { get; set; }
+        public List<string> Inventory { get; set; }
+    }
+
+    public class GameStateSave
+    {
+        public PlayerSaveData PlayerData { get; set; }
+        public MapSaveData MapData { get; set; }
+    }
+}
+
---
+++ TheGiverOnMars/Constants.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TheGiverOnMars.Managers;
+
+namespace TheGiverOnMars
+{
+    public static class Constants
+    {
+        public static GraphicsDeviceManager Graphics { get; set; }
+        public static SpriteBatch SpriteBatch { get; set; }
+        public static ContentManager Content { get; set; }
+        public static TheGiverOnMars Game { get; set; }
+
+        public static int ScreenHeight;
+        public static int ScreenWidth;
+
+        public static States.State CurrentState;
+        public static States.State NextState;
+
+        public static SceneManager SceneManager;
+
+        public static SpriteFont InventoryStackFont { get; set; }
+    }
+}
